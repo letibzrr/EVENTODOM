79614af6e559b14b4d0ec4ef45454f9f41613754
letibzrr <leticiabezerracontato@gmail.com>
2023-11-16 12:17:24 -0300

    correção

--- EventosDOM/projeto_04/app.js
+++ EventosDOM/projeto_04/app.js
@@ -1,4 +1,7 @@
 function darkMode(){
     const pagina = document.body
     pagina.classList.toggle("dark-mode")
+    const isDarkMode = document.body.classList.contains("dark-mode")
+    let textoBotao = isDarkMode ? "Modo Claro" : "Modo Escuro";
+    document.querySelector("button").textContent = textoBotao;
 }
